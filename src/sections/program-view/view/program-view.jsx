f81d6f4594924a102d0988299bff190d621a5de4
Wick <vic_the_gunners@hotmail.com>
2024-07-01 13:57:11 +0700

    edit docker

--- src/sections/program-view/view/program-view.jsx
+++ src/sections/program-view/view/program-view.jsx
@@ -24,7 +24,7 @@ export default function ProgramView() {
     useEffect(() => {
         const fetchPrograms = async () => {
             try {
-                const response = await fetch('http://18.143.102.26/program');
+                const response = await fetch('http://3.1.6.34/program');
                 if (!response.ok) {
                     throw new Error('Failed to fetch programs');
                 }
